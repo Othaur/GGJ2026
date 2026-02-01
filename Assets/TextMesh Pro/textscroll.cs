6558c6acb88dd71aa882926378a5abb778c5dea7
Francis De Guzman <yrk502@usask.ca>
2026-01-31 19:11:38 -0600

    Added feature for scroll: second click prints all dialogue

--- Assets/TextMesh Pro/textscroll.cs
+++ Assets/TextMesh Pro/textscroll.cs
@@ -9,25 +9,45 @@ using TMPro;
 public class textscroll : MonoBehaviour
 {
     [Header("Text Settings")]
-    [SerializeField] [TextArea] private string[] itemInfo;
+    [SerializeField, TextArea] private string[] itemInfo;
     [SerializeField] private float textSpeed = 0.01f;
 
     [Header("UI Elements")]
     [SerializeField] private TextMeshProUGUI itemInfoText;
+
     private int currentDisplayingText = 0;
 
-    public void ActivateText()
+    private Coroutine typingCoroutine;
+    private bool isTyping = false;
 
+    public void ActivateText()
     {
-        StartCoroutine(AnimateText());
+        // If already typing, complete text instantly
+        if (isTyping)
+        {
+            StopCoroutine(typingCoroutine);
+            itemInfoText.text = itemInfo[currentDisplayingText];
+            isTyping = false;
+            return;
+        }
+
+        // Otherwise start typing
+        typingCoroutine = StartCoroutine(AnimateText());
     }
 
     IEnumerator AnimateText()
     {
-        for (int i = 0; i < itemInfo[currentDisplayingText].Length + 1; i++)
-        {   
-            itemInfoText.text = itemInfo[currentDisplayingText].Substring(0, i);
+        isTyping = true;
+        itemInfoText.text = "";
+
+        string fullText = itemInfo[currentDisplayingText];
+
+        for (int i = 0; i <= fullText.Length; i++)
+        {
+            itemInfoText.text = fullText.Substring(0, i);
             yield return new WaitForSeconds(textSpeed);
         }
+
+        isTyping = false;
     }
 }
