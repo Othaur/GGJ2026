d9ed1a73e108fefc3da0a3cc85cf2c60ca104518
Francis De Guzman <yrk502@usask.ca>
2026-01-31 18:07:50 -0600

    Added textscroll.cs to Assets/TextMesh Pro

---
+++ Assets/TextMesh Pro/textscroll.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+using TMPro;
+
+public class textscroll : MonoBehaviour
+{
+    [Header("Text Settings")]
+    [SerializeField] [TextArea] private string[] itemInfo;
+    [SerializeField] private float textSpeed = 0.01f;
+
+    [Header("UI Elements")]
+    [SerializeField] private TextMeshProUGUI itemInfoText;
+    private int currentDisplayingText = 0;
+
+    public void ActivateText()
+
+    {
+        StartCoroutine(AnimateText());
+    }
+
+    IEnumerator AnimateText()
+    {
+        for (int i = 0; i < itemInfo[currentDisplayingText].Length + 1; i++)
+        {   
+            itemInfoText.text = itemInfo[currentDisplayingText].Substring(0, i);
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+}
